7591309f63b5fcdcaab15baabb8e0b1ec0606044
sq <sq11235@163.com>
2020-04-08 23:53:51 +0800

    添加了322,931,983

--- algorithm/leetcode/dp/322-动态规划-median-零钱兑换.py
+++ algorithm/leetcode/dp/322-动态规划-median-零钱兑换.py
@@ -11,4 +11,32 @@
 输出: -1
 
 分析:
+dp[i]表示凑成总金额i时所用的最少硬币个数.//状态定义
+
+dp[i]=min(dp[i-coins[0]]+1,dp[i-coins[1],...,dp[i-coins[n]]+1]//状态转移
+
+dp[coin[0]]=1,..dp[coins[n]]=1,
+
+dp[j]=-1(j<coins[0])
 """
+
+class Solution:
+    def coinChange(self, coins, amount: int) -> int:
+        n=len(coins)
+
+        dp=[float('inf')]*(amount+1)
+        dp[0]=0
+
+        
+        for c in coins:
+            for i in range(amount+1):
+                if i>=c:
+                    dp[i]=min(dp[i],dp[i-c]+1)
+        
+        return dp[amount] if dp[amount]!=float('inf') else -1
+
+if __name__=='__main__':
+    s=Solution()
+    coins = [1, 2, 5]
+    amount = 11
+    print(s.coinChange(coins,amount))
